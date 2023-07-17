41c9ee5e9eec68cec1aa5c678c818ee03423cfb3
Calise425 <caitlin.norris1998@gmail.com>
2023-07-17 17:29:59 -0600

    code is passing all testsgit add .

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 // Build an apiRouter using express Router
-
+const apiRouter = require('express').Router();
 
 // Import the database adapter functions from the db
-
+const { getOpenReports, createReport, closeReport, createReportComment } = require('../db');
 
 /**
  * Set up a GET request for /reports
@@ -12,7 +12,14 @@
  * - on success, it should send back an object like { reports: theReports }
  * - on caught error, call next(error)
  */
-
+apiRouter.get('/reports', async (req, res, next)=>{
+  try {
+    const openReports = await getOpenReports();
+    res.send({reports: openReports})
+  } catch ({name, message}) {
+    next({ name, message });
+  }
+})
 
 
 /**
@@ -23,6 +30,14 @@
  * - on success, it should send back the object returned by createReport
  * - on caught error, call next(error)
  */
+apiRouter.post('/reports', async (req, res, next)=>{
+  try {
+    const report = await createReport(req.body);
+    res.send(report)
+  } catch ({name, message}) {
+    next({ name, message });
+  }
+})
 
 
 
@@ -36,6 +51,16 @@
  * - on caught error, call next(error)
  */
 
+apiRouter.delete('/reports/:reportId', async (req, res, next)=>{
+  try {
+    const {password} = req.body;
+    const {reportId} = req.params;
+    const result = await closeReport(reportId, password);
+    res.send(result)
+  } catch ({name, message}) {
+    next({ name, message });
+  }
+})
 
 
 /**
@@ -47,7 +72,17 @@
  * - on success, it should send back the object returned by createReportComment
  * - on caught error, call next(error)
  */
+apiRouter.post('/reports/:reportId/comments', async (req, res, next)=>{
+  try {
+    const {reportId} = req.params
+    const result = await createReportComment(reportId, req.body);
+    res.send(result)
+  } catch ({name, message}) {
+    next({ name, message });
+  }
+})
 
 
 
 // Export the apiRouter
+module.exports = apiRouter
--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 // Use the dotenv package, to create environment variables
-require('dotenv').config();
-
 // Create a constant variable, PORT, based on what's in process.env.PORT or fallback to 3000
-
 // Import express, and create a server
-
 // Require morgan and body-parser middleware
-
 // Have the server use morgan with setting 'dev'
-
 // Import cors 
 // Have the server use cors()
-
 // Have the server use bodyParser.json()
-
 // Have the server use your api router with prefix '/api'
-
 // Import the client from your db/index.js
 
-// Create custom 404 handler that sets the status code to 404.
 
-// Create custom error handling that sets the status code to 500
-// and returns the error as an object
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const apiRouter = require('./api');
+const { client } = require('./db');
 
+// Middleware
+server.use(morgan('dev'));
+server.use(cors());
+server.use(bodyParser.json());
 
-// Start the server listening on port PORT
-// On success, connect to the database
+// API Router
+server.use('/api', apiRouter);
+
+// Custom error handling
+server.use((req, res, next) => {
+  res.status(404).send('404: Page not found');
+});
+
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('500: Internal Server Error');
+});
+
+// Start the server
+server.listen(PORT, async () => {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+    console.log('The server is up on port', PORT);
+  } catch (err) {
+    console.error('Failed to connect to the database:', err);
+  }
+});
